14d1c9b21f4bb8356102eb20817d602e68cefaa4
shizhh <shizhh@shanghaitech.edu.cn>
2024-04-09 06:23:23 -0700

    fix path bug

--- tmux_web/launcher.py
+++ tmux_web/launcher.py
@@ -4,6 +4,7 @@ import os
 
 def launch_streamlit():
     print("Launching Streamlit app...")
-    app_path = './app.py'
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    app_path = os.path.join(dir_path, 'app.py')
     command = f"streamlit run {app_path}"
     subprocess.run(command, shell=True)
