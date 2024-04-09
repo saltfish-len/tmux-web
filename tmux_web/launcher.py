e887c81c7dfddfcca751a0f3d9649d9a405cdf77
shizhh <shizhh@shanghaitech.edu.cn>
2024-04-09 06:17:08 -0700

    update use relative path

--- tmux_web/launcher.py
+++ tmux_web/launcher.py
@@ -4,6 +4,6 @@ import os
 
 def launch_streamlit():
     print("Launching Streamlit app...")
-    app_path = os.path.join(os.getcwd(), 'app.py')
+    app_path = './app.py'
     command = f"streamlit run {app_path}"
     subprocess.run(command, shell=True)
